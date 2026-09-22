6b17d43b66f01577d404b374b10cd7bdd47f4148
ChanJoos <challyju@gmail.com>
2026-09-23 01:42:20 +0900

    B5: correct building approaches to canonical 64x32 tile centers

--- app/src/main/java/com/projectdark/mobile/world/MillesProductionCollision.java
+++ app/src/main/java/com/projectdark/mobile/world/MillesProductionCollision.java
@@ -73,10 +73,10 @@ public final class MillesProductionCollision {
     // Reachable south-side interaction anchors for the B1/B5 building placement.
     List<Approach> a=new ArrayList<>();
     a.add(new Approach("potion_shop",320f,496f));
-    a.add(new Approach("weapon_shop",768f,384f));
+    a.add(new Approach("weapon_shop",736f,384f));
     a.add(new Approach("general_shop",1120f,448f));
     a.add(new Approach("church",1536f,560f));
-    a.add(new Approach("inn",1984f,736f));
+    a.add(new Approach("inn",2016f,736f));
     ENTRANCES=Collections.unmodifiableList(a);
   }
 
