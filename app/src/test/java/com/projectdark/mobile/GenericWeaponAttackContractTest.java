e9f7913b5b9eb9736cc7ddc4cb9b3bb6cb14555e
ChanJoos <challyju@gmail.com>
2026-09-24 01:14:35 +0900

    Fix generic weapon attack contract field reference

--- app/src/test/java/com/projectdark/mobile/GenericWeaponAttackContractTest.java
+++ app/src/test/java/com/projectdark/mobile/GenericWeaponAttackContractTest.java
@@ -4,8 +4,8 @@ public final class GenericWeaponAttackContractTest {
  @Test public void alternateWeaponsRemainSwingAndVisualWeaponSlot(){
   RpgProgressionState r=new RpgProgressionState();
   for(String id:new String[]{"IT_TEST_WEAPON_MW002","IT_TEST_WEAPON_MW003"}){
-   RpgProgressionState.ItemDefinition d=r.itemDefinitions().get(id);assertNotNull(d);assertEquals(AnimationAction.SWING,d.animationAction);assertEquals(CharacterVisualBinding.VisualSlot.WEAPON,CharacterVisualBinding.visualSlotForAppearance(d.appearanceId));
-   r.equip(id);CharacterVisualBinding b=CharacterVisualBinding.from(r);assertEquals(d.appearanceId,b.weaponVisualRef());assertTrue(CharacterRenderer.usesSourceActionPose(CharacterRenderer.State.ATTACK,d.animationAction));
+   RpgProgressionState.ItemDefinition d=r.itemDefinitions().get(id);assertNotNull(d);assertEquals(CharacterVisualBinding.VisualSlot.WEAPON,CharacterVisualBinding.visualSlotForAppearance(d.appearanceId));
+   r.equip(id);CharacterVisualBinding b=CharacterVisualBinding.from(r);assertEquals(d.appearanceId,b.weaponVisualRef());assertTrue(CharacterRenderer.usesSourceActionPose(CharacterRenderer.State.ATTACK,AnimationAction.SWING));
   }
  }
 }
