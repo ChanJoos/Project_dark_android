2bc934374fd4bf4dd51130364dca1a7f1272a2eb
ChanJoos <challyju@gmail.com>
2026-09-24 01:20:45 +0900

    Cover expanded weapon catalog in attack contract

--- app/src/test/java/com/projectdark/mobile/GenericWeaponAttackContractTest.java
+++ app/src/test/java/com/projectdark/mobile/GenericWeaponAttackContractTest.java
@@ -3,7 +3,7 @@ import static org.junit.Assert.*;import org.junit.Test;
 public final class GenericWeaponAttackContractTest {
  @Test public void alternateWeaponsRemainSwingAndVisualWeaponSlot(){
   RpgProgressionState r=new RpgProgressionState();
-  for(String id:new String[]{"IT_TEST_WEAPON_MW002","IT_TEST_WEAPON_MW003"}){
+  for(String id:new String[]{"IT_TEST_WEAPON_MW002","IT_TEST_WEAPON_MW003","IT_TEST_WEAPON_MW004","IT_TEST_WEAPON_MW005","IT_TEST_WEAPON_MW006","IT_TEST_WEAPON_MW007","IT_TEST_WEAPON_MW008","IT_TEST_WEAPON_MW009","IT_TEST_WEAPON_MW010"}){
    RpgProgressionState.ItemDefinition d=r.itemDefinitions().get(id);assertNotNull(d);assertEquals(CharacterVisualBinding.VisualSlot.WEAPON,CharacterVisualBinding.visualSlotForAppearance(d.appearanceId));
    r.equip(id);CharacterVisualBinding b=CharacterVisualBinding.from(r);assertEquals(d.appearanceId,b.weaponVisualRef());assertTrue(CharacterRenderer.usesSourceActionPose(CharacterRenderer.State.ATTACK,AnimationAction.SWING));
   }
