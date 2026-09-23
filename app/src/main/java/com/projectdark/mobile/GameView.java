83df9874e84c0c5776f72848d3ea5029f73d3731
ChanJoos <challyju@gmail.com>
2026-09-24 00:45:04 +0900

    Use real newline for inventory page field

--- app/src/main/java/com/projectdark/mobile/GameView.java
+++ app/src/main/java/com/projectdark/mobile/GameView.java
@@ -46,7 +46,8 @@ public final class GameView extends View {
 
   private float scale=1,ox,oy,vx,vy;
   private float knobX=JOY_X,knobY=JOY_Y;
-  private boolean joy,running,inventoryOpen,statsOpen,equipmentOpen;\n  private int inventoryPage=0;
+  private boolean joy,running,inventoryOpen,statsOpen,equipmentOpen;
+  private int inventoryPage=0;
   private float checkpointClock;
   private boolean saveWarningShown;
   private long savedLedgerSequence;
