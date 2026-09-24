462dcdf1f2054fcfe7780f876881f242cce0bfe0
ChanJoos <challyju@gmail.com>
2026-09-24 19:07:58 +0900

    Keep recorded Milles scene at native viewport size

--- app/src/main/java/com/projectdark/mobile/world/AdaptedMillesMapRenderer.java
+++ app/src/main/java/com/projectdark/mobile/world/AdaptedMillesMapRenderer.java
@@ -44,7 +44,7 @@ public final class AdaptedMillesMapRenderer {
     float left=WorldDef.PLAYER_SPAWN_X-w.camera().anchorX();
     float top=WorldDef.PLAYER_SPAWN_Y-w.camera().anchorY();
     WorldCameraTransform.Point p=w.worldToScreen(left,top);
-    RectF dst=new RectF(p.x,p.y,p.x+c.getWidth(),p.y+c.getHeight());
+    RectF dst=new RectF(p.x,p.y,p.x+scene.getWidth(),p.y+scene.getHeight());
     c.drawBitmap(scene,null,dst,pixelPaint);
   }
 
